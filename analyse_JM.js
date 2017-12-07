d56644c1b06a892eff3c33cfd6a045977b10a12e
Jean Mainguy <jean.mainguy@outlook.fr>
2017-12-07 17:14:07 +0100

    Two fonctionnelle

--- analyse_JM.js
+++ analyse_JM.js
@@ -32,35 +32,35 @@
  * @param {TRaster} img - Input image
  * @param {boolean} copy - Copy mode
  * @return {type} A set of Regions Of Interest (ROI)
- * @author TODO
+ * @author Jean Mainguy
  */
 const labelling = function (img,copy=true) {
-  // TODO
+
   let raster = img.getRaster();
-  console.log(`labelling`);
   union_find = {label_cmpt:0};
-  let label_img = new Array(img.height*img.width); //sempty array that will contain labels of the pixels
-  for(let i=0;i<label_img.length;i++){
-      if(raster.get(i) == 255){
-          console.log(label_img[i]);
-          label_me(i, label_img, img.width);
-          console.log(label_img[i]);
-      }
-  }
+
+  let label_img = new Array(img.height*img.width); //empty array that will contain labels of the pixels
+
+  // First Pass
+  raster.pixelData.map(function(pix, i){
+    pix == 255 ? label_pix(i, label_img, raster.width, union_find) : undefined;
+  });
+
+  // Second Pass
+  label_img = label_img.map(function(label, i){return union_find[label_img[i]]})
   console.log(union_find);
   return label_img;
 }
 
-const label_me = function(i, label_img, w) {
+const label_pix = function(i, label_img, w, union_find) {
+
     let label_up = label_img[i-w]; //store label of pixel above i. may bereal label or undefined/null
     let label_left = i % w == 0 ? undefined : label_img[i-1]; // store label of pixel right before i. may be real label or undefined/null
-    console.log('up is ', label_up);
-    console.log('left is ', label_left);
 
     let label_i = label_up !== undefined && label_left !== undefined ? (
         notify_union_find(label_up, label_left, union_find), //va notifier meme si les deux label sont pareil donc c'est pas top..
         label_up > label_left ? label_left : label_up
-        // notification to
+
     ) : (
         label_up == undefined && label_left == undefined ? (
             new_label(union_find)
@@ -68,12 +68,11 @@ const label_me = function(i, label_img, w) {
             label_up == undefined ? label_left : label_up
         )
     );
-
     label_img[i] = label_i;
 }
 
 const notify_union_find = function(labelA, labelB, union_find){
-    labelA < labelB ? (union_find[labelB] = labelA) : (union_find[labelA] = labelB);
+    labelA < labelB ? (union_find[labelB] = union_find[labelA]) : (union_find[labelA] = union_find[labelB]);
 }
 const new_label = function(union_find){
     union_find.label_cmpt ++;
@@ -98,65 +97,83 @@ const measure = function (params) {
   }
 }
 
+const show = function(result, h, w){
+    let line = "";
+    let show = "";
+    for(let i=0;i<(h*w);i++){
 
-
-
-data = [
-  0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
-  0, 255, 0, 0, 0, 255, 255, 0, 0, 0,
-  0, 255, 255, 0, 0, 255, 255, 0, 0, 0,
-  0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
-  0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
-  0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 255, 255, 0, 0, 0,
-  0, 0, 0, 255, 255, 0, 0, 0, 0, 0,
-  0, 0, 255, 255, 0, 0, 0, 0, 0, 0];
-
-let img;
-img = new T.Image('uint8',10,10);
-
-
-// data = [
-//     1, 1, 0,
-//     0, 0, 0,
-//     0, 0, 1,
-//     1, 0, 1,
-// ]
-//
-// img = new T.Image('uint8',3,4);
-
-
-img.setPixels(data);
-
-
-result = labelling(img);
-
-
-// PROVISOIRE PERMET DE PRINTER LIMAGE DANS LA CONSOLE
-let line = "";
-let show = "";
-for(let i=0;i<(img.height*img.width);i++){
-
-    if(i % img.width == 0){
-        console.log(line);
-        console.log('  ______________')
-        line = "";
-        // console.log(i);
+        if(i % w == 0){
+            console.log(line);
+            console.log('  ______________')
+            line = "";
+            // console.log(i);
+        }
+        if( result[i] == undefined){
+            show = 'X';
+        }
+        else{
+            show = result[i];
+        }
+        line += "   " + show;
     }
-    if( result[i] == undefined){
-        show = 'X';
-    }
-    else{
-        show = result[i];
-    }
-    line += "   " + show;
+    console.log(line);
 }
-console.log(line);
 
-let win0 = new T.Window('Boats');
+console.log("labelling 2 pass");
+
+
+let img = new T.Image('uint8',100,100);
+img.setPixels(img_binary);
+let win0 = new T.Window('Binary');
 let view0 = T.view(img.getRaster());
 // Create the window content from the view
 win0.addView(view0);
 // Add the window to the DOM and display it
 win0.addToDOM('workspace');
+
+
+// data = [
+//   0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
+//   0, 255, 0, 0, 0, 255, 255, 0, 0, 0,
+//   0, 255, 255, 0, 0, 255, 255, 0, 0, 0,
+//   0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
+//   0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
+//   0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
+//   0, 0, 0, 0, 0, 255, 0, 0, 0, 0,
+//   0, 0, 0, 0, 0, 255, 255, 0, 0, 0,
+//   0, 0, 0, 255, 255, 0, 0, 0, 0, 0,
+//   0, 0, 255, 255, 0, 0, 0, 0, 0, 0];
+//
+// let img;
+// img = new T.Image('uint8',10,10);
+
+
+// img.setPixels(data);
+
+
+result = labelling(img);
+for(let i=0;i<result.length;i++){
+    if (result[i] == undefined){
+        result[i] = 255;
+    }
+}
+// show(result, img.height, img.width)
+// result = result.map(function(i){i*50})
+show(result, img.height, img.width)
+let img_result = new T.Image('uint8',100,100);
+img.setPixels(result);
+let win1 = new T.Window('Binary result');
+let view1 = T.view(img_result.getRaster());
+// Create the window content from the view
+win1.addView(view1);
+// Add the window to the DOM and display it
+win1.addToDOM('workspace');
+// show(result, img.height, img.width)
+
+
+// let win1 = new T.Window('Labbeling');
+// let view1 = T.view(img.getRaster());
+// Create the window content from the view
+// win0.addView(view0);
+// // Add the window to the DOM and display it
+// win0.addToDOM('workspace');
