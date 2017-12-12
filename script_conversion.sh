596a26e4c32bbc2056be5c4df5edc59349cd276d
Jean Mainguy <jean.mainguy@outlook.fr>
2017-12-12 19:57:47 +0100

    fixing bug

--- script_conversion.sh
+++ script_conversion.sh
@@ -7,5 +7,6 @@ name_array="${name_array/.txt/}"
 
 
 echo "let $name_array = [" > $img_js
-sed 's/\t/,/g' $img_txt >> $img_js
-echo "];" >> $img_js
+sed 's/\t/,/g' $img_txt | sed -e 's/$/,/' | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' | sed -e '$s/,$/];/'>> $img_js
+tail $img_js
+# echo "];" >> $img_js
