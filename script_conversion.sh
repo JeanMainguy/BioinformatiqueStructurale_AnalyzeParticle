0062fa9b72152d5aa7e87dda6edd90fe08cf87f1
Jean Mainguy <jean.mainguy@outlook.fr>
2017-12-12 19:17:10 +0100

    amelioration du script

--- script_conversion.sh
+++ script_conversion.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-img_txt=$1
+img_txt=$1  #argument a donner
 img_js="${img_txt/.txt/.js}"
+name_array="${img_txt/[a-zA-Z0-9]*\//}"
+name_array="${name_array/.txt/}"
 
-echo "let test_binary = [" > $img_js
-sed 's/\t/,/g' $img_txt >> $img_js 
-echo "];" >> $img_js 
+
+echo "let $name_array = [" > $img_js
+sed 's/\t/,/g' $img_txt >> $img_js
+echo "];" >> $img_js
