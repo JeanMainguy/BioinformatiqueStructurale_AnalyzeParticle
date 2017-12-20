3b7d834ecb37eec34062080abca7c824d1af9cac
Jean Mainguy <jean.mainguy@outlook.fr>
2017-12-20 14:20:28 +0100

    findExtrem is added

--- examples/test_2_pass.js
+++ examples/test_2_pass.js
@@ -1,8 +1,19 @@
 console.log("labelling 2 pass");
 
+data = [
+  0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
+  0, 255, 0, 0, 0, 255, 255, 0, 0, 0,
+  0, 255, 255, 0, 0, 255, 255, 0, 0, 0,
+  0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
+  0, 255, 255, 255, 255, 255, 255, 0, 0, 0,
+  0, 255, 255, 255, 255, 255, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 255, 0, 0, 0, 0,
+  0, 0, 0,   0,     0,  255, 255, 0, 0, 0,
+  0, 0, 0,   255, 255,   0, 0, 0, 0, 0,
+  0, 0, 255, 255, 0, 0, 0, 0, 0, 0];
 
-let img = new T.Image('uint8',256,254);
-img.setPixels(blobs_bin256x254);
+let img = new T.Image('uint8',10,10);
+img.setPixels(data);
 let win0 = new T.Window('Binary');
 let view0 = T.view(img.getRaster());
 // Create the window content from the view
@@ -20,20 +31,31 @@ result = labelling(img);
 console.log("RESULT ");
 console.log(result);
 
-let particle_list = convert_index_to_xy_particle(result, img.getRaster());
-
-console.log("AREA");
-console.log(area(particle_list[1]));
-console.log(particle_list[1]);
-console.log(particle_list[1].length);
-
-console.log(bundingRectangle(particle_list[1]));
+console.log(getListParticle(result));
+// let particle_list = convert_index_to_xy_particle(result, img.getRaster());
+// particule_list = Object.values(particle_list);
+// console.log("LENGTH particule list",particule_list.length);
+// console.log(particle_list);
+// for(let i=0; i<particule_list.length; i++){
+//     // console.log(particle_list[i]);
+// //     // console.log(particle_list[k]);
+//     console.log(i, particle_list[i].length);
+// }
+//
+// console.log("LENGTH", Object.keys(particle_list).length);
+// console.log("AREA");
+// // console.log(area(particle_list[1]));
+// console.log(particle_list[1]);
+// console.log(particle_list[1].length);
+//
+// console.log(boundingRectangle(particle_list[1]));
 
 
 // show(result, img.height, img.width)
 // result = result.map(function(i){i*50})
 // show(result, img.height, img.width)
 let img_result = new T.Image('uint8',img.width,img.height);
+// result.fill(0xff1994e4)
 img_result.setPixels(result);
 let win1 = new T.Window('Binary result');
 let view1 = T.view(img_result.getRaster());
@@ -70,3 +92,6 @@ win1.addToDOM('workspace');
 
 
 // img.setPixels(data);
+
+
+console.log(findExtrem([[1,5], [6,8], [-87,51]]));
--- src/analyse_two_pass.js
+++ src/analyse_two_pass.js
@@ -36,51 +36,61 @@
  */
 const labelling = function (img,copy=true) {
 
+
+  function label_pix (i, label_img, w, union_find) {
+
+      let label_up = i-w < 0 ? 0 : label_img[i-w]; //store label of pixel above i. may bereal label or undefined/null
+      let label_left = i % w == 0 ? 0 : label_img[i-1]; // store label of pixel right before i. may be real label or undefined/null
+
+      let label_i = label_up !== 0 && label_left !== 0 ? ( //
+              notify_union_find(label_up, label_left, union_find), //if the label up and label rigth are labbeled then the union find structure is notify
+              label_up > label_left ? label_left : label_up  // label_i (label of the current pixel analysed) take the smalest label of the neigbor
+          ) : (
+              label_up == 0 && label_left == 0 ? (
+                  new_label(union_find) // if the two the pixels up and left are backgrounf pixel then a new label is provided to label_i
+                  ) : (
+                  label_up == 0 ? label_left : label_up // and finally if the only one neighbor is label, label_i get this label
+                      )
+              );
+      label_img[i] = label_i;
+  }
+
+  function notify_union_find(labelA, labelB, union_find){
+      labelA < labelB ? (union_find[labelB] = union_find[labelA]) : (union_find[labelA] = union_find[labelB]);
+  }
+
+  function new_label(union_find){
+      union_find.label_cmpt ++;
+      union_find[union_find.label_cmpt] = union_find.label_cmpt;
+      return union_find.label_cmpt;
+  }
+
+
   let raster = img.getRaster();
   union_find = {label_cmpt:0, 0:0};
 
   let label_img = new Array(img.height*img.width); //empty array that will contain labels of the pixels
-  label_img.fill(0)
+  label_img.fill(0);
   // First Pass
   raster.pixelData.forEach(function(pix, i){
     pix == 255 ? label_pix(i, label_img, raster.width, union_find) : undefined;
-});
+  });
 
   // Second Pass
-  label_img = label_img.map(function(label, i){return union_find[label_img[i]]})
+  label_img = label_img.map((label, i) => union_find[label_img[i]]);
+
+  label_raster = img.getRaster();
+  label_raster.pixelData = label_img;
 
 
-
-  return label_img;
+  return label_raster;
 }
 
-const label_pix = function(i, label_img, w, union_find) {
 
-    let label_up = i-w < 0 ? 0 : label_img[i-w]; //store label of pixel above i. may bereal label or undefined/null
-    let label_left = i % w == 0 ? 0 : label_img[i-1]; // store label of pixel right before i. may be real label or undefined/null
 
-    let label_i = label_up !== 0 && label_left !== 0 ? ( //
-            notify_union_find(label_up, label_left, union_find), //if the label up and label rigth are labbeled then the union find structure is notify
-            label_up > label_left ? label_left : label_up  // label_i (label of the current pixel analysed) take the smalest label of the neigbor
-        ) : (
-            label_up == 0 && label_left == 0 ? (
-                new_label(union_find) // if the two the pixels up and left are backgrounf pixel then a new label is provided to label_i
-                ) : (
-                label_up == 0 ? label_left : label_up // and finally if the only one neighbor is label, label_i get this label
-                    )
-            );
-    label_img[i] = label_i;
-}
 
-const notify_union_find = function(labelA, labelB, union_find){
-    labelA < labelB ? (union_find[labelB] = union_find[labelA]) : (union_find[labelA] = union_find[labelB]);
-}
 
-const new_label = function(union_find){
-    union_find.label_cmpt ++;
-    union_find[union_find.label_cmpt] = union_find.label_cmpt;
-    return union_find.label_cmpt;
-}
+
 
 /**
  * Measure a set of Regions Of Interest (ROI)
@@ -104,16 +114,13 @@ const convert_index_to_xy_particle = function (labbeling_img, raster){
     function is_background(index){
         return index != 0;
     }
-    function add_xy_to_dict(index, label, raster){
+    function add_xy_to_dict(index, label){
 
-        // if(listparticle.hasOwnProperty(label)){
-        //     listparticle[label].push(raster.xy(index));
-        // }
-        // else {
-        //     listparticle[label] =  new Array(raster.xy(index));
-        // }
         listparticle.hasOwnProperty(label) ? ( listparticle[label].push(raster.xy(index)) ):( listparticle[label] =  new Array(raster.xy(index)));
     }
+
+
+    // let listparticle = new Object();
     let listparticle = new Object();
     labbeling_img.forEach(function(label, index){
         // console.log(label);
@@ -123,12 +130,48 @@ const convert_index_to_xy_particle = function (labbeling_img, raster){
     return listparticle;
 }
 
+const getListParticle = function(labbeled_raster){
+  function getParticle(list_p, label, i ){
+    return list_p = label < 1 ? (list_p) :(
+    index = labels.indexOf(label),
+    index != -1 ? label_array[index].push(labbeled_raster.xy(i)) : label_array.push([labbeled_raster.xy(i)]),
+    list_p);
+  }
+  let label_list = new Array();
+  let label_array = labbeled_raster.pixelData;
+  let list_p = new Array();
+
+  label_array.reduce(function(list_p, label, i){
+    console.log("label list", label_list);
+    return list_p = label < 1 ? (list_p) :(
+      console.log("label",label , "  list_p", list_p, " "),
+    index = label_list.indexOf(label),
+    console.log("index", index),
+    // if (index == -1){
+    //   console.log("index is -1 donc on ajoute une nouevlle list à label_array");
+    //   list_p.push([labbeled_raster.xy(i)])
+    // }
+    // else{
+    //   list_p[index].push(labbeled_raster.xy(i))
+    // }
+    index != -1 ? list_p[index].push(labbeled_raster.xy(i)) : (
+                list_p.push([labbeled_raster.xy(i)]),
+                label_list.push(label)),
+    list_p);
+  }, list_p);
+  // for (let i; i<label_array.length; i++){
+  //   if (label_array)
+  // };
+  return list_p
+}
+
+
 
 const area = function (particule){
   return particule.reduce( (number) => number+=1);
 }
 
-const bundingRectangle = function(particule){
+const boundingRectangle = function(particule){
   console.log("particule",particule);
   let extrem = {xmin:particule[0][0], xmax:particule[0][0], ymin:particule[0][1], ymax:particule[0][1]};
 
@@ -142,6 +185,21 @@ const bundingRectangle = function(particule){
   }, extrem);
   console.log("extrem", extrem);
   return {width:extrem.xmax-extrem.xmin+1, height:extrem.ymax-extrem.ymin+1, bx:extrem.xmin, by:extrem.ymin }
+}
+
+const findExtrem = function(particule){
+  //particule is a list of coord
+  let extrem = {xmin:particule[0][0], xmax:particule[0][0], ymin:particule[0][1], ymax:particule[0][1]};
+
+
+  particule.reduce(function(extrem,coord){
+    extrem.xmin = extrem.xmin > coord[0] ? coord[0] : extrem.xmin;
+    extrem.xmax = extrem.xmax < coord[0] ? coord[0] : extrem.xmax;
+    extrem.ymin = extrem.ymin > coord[1] ? coord[1] : extrem.ymin;
+    extrem.ymax = extrem.ymax < coord[1] ? coord[1] : extrem.ymax;
+    return extrem;
+  }, extrem);
+  return extrem
 }
 
 const show = function(result, h, w){
