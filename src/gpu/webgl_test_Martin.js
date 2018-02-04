d3dcf1339e4f94214ece417cabc03882890c9097
Arkhaan <martin.binet@laposte.net>
2018-02-04 22:54:29 +0100

    working on step1

--- src/gpu/webgl_test_Martin.js
+++ src/gpu/webgl_test_Martin.js
@@ -57,65 +57,113 @@ let src_fs = `#version 300 es
     uniform float u_kernel[9];
     uniform sampler2D u_raster;
     uniform vec2 u_textureSize;
+    uniform int step;
 
     // Declare an output for the fragment shader
     out vec4 outColor;
 
-    float rand(vec2 co){
-        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-    }
-
     void main() {
-        // vec2 st = vec2(v_texCoord.x, v_texCoord.y);
-        // vec4(rand(st), rand(1.0-st), rand(st+1.0), 1.0);
+      vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
 
-        // compute 1 pixel in texture coordinates.
-        vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
-
-        if(texture(u_raster, v_texCoord).r < 1.0 && texture(u_raster, v_texCoord + vec2(onePixel.x, 0.0)).r < 1.0){
-            outColor = vec4(1.0, 0.0, 1.0, 1.0);
+      if(texture(u_raster, v_texCoord).r == 0.0){
+        if(step == 0){
+          outColor = vec4(0.0, v_texCoord.x, v_texCoord.y, 1.0);
         }
-        // outColor = vec4(rand(st) - texture(u_raster, v_texCoord).rgb, 1.0);
-        // if(v_texCoord.x>0.5){
-        //     outColor = texture(u_raster, v_texCoord);
-        // }
+        else if(step == 1){
+          float going = 1.0;
+          float y = 0.0;
+          while(going == 1.0){
+            if(texture(u_raster, v_texCoord - vec2(0.0, onePixel.y*y)).r == 0.0){
+              y++;
+            }
+            else{
+              going = 0.0;
+              outColor = texture(u_raster, v_texCoord - vec2(0.0, onePixel.y*y));
+            }
+          }
+        }
+        else{
+          outColor = texture(u_raster, v_texCoord);
+        }
+      }
+      else{
+        outColor = texture(u_raster, v_texCoord);
+      }
 }` ;
+
+// outColor = texture(u_raster, v_texCoord + vec2(0.0, onePixel.y));
+
+// vec2 st = vec2(v_texCoord.x, v_texCoord.y);
+// vec4(rand(st), rand(1.0-st), rand(st+1.0), 1.0);
+
+// compute 1 pixel in texture coordinates.
+// vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
+//
+// if(texture(u_raster, v_texCoord).r < 1.0 && texture(u_raster, v_texCoord + vec2(onePixel.x, 0.0)).r < 1.0){
+//     outColor = vec4(1.0, 0.0, 0.0, 1.0);
+// }
+// outColor = vec4(rand(st) - texture(u_raster, v_texCoord).rgb, 1.0);
+// if(v_texCoord.x>0.5){
+//      outColor = texture(u_raster, v_texCoord);
+// }
 
 let program = gpu.createProgram(gpuEnv,src_vs,src_fs);
 
 w = raster.width;
 h = raster.height;
 
-let gproc = gpu.createGPU(gpuEnv,w,h);
-
-gproc.geometry(gpu.rectangle(raster.width,raster.height));
-
-// gproc.geometry({
-//     type : 'TRIANGLE_STRIP',
-//     num : 4,
-//     vertices : new Float32Array([
-//         0.0,0.0,0.0,0.0,
-//         0.0,h  ,0.0,1.0,
-//         w  ,0.0,1.0,0.0,
-//         w  ,h  ,1.0,1.0])
-// })
-
-
-gproc.attribute('a_vertex',2,'float', 16,0)   // X, Y
-    .attribute('a_texCoord',2, 'float', 16, 8)  // S, T
-    .packWith(program)                         // VAO
-gproc.texture(raster,0) ;
-
-
-gproc.clearCanvas([0.0,1.0,0.0,1.0]);
-
-gproc.preprocess()
-    .uniform('u_resolution',
+let gprocStep0 = gpu.createGPU(gpuEnv,w,h)
+  .geometry(gpu.rectangle(raster.width,raster.height))
+  .attribute('a_vertex',2,'float', 16,0)   // X, Y
+  .attribute('a_texCoord',2, 'float', 16, 8)  // S, T
+  .packWith(program)                         // VAO
+  .texture(raster, textUnit = 1)
+  .redirectTo('fbo1','float32',0)
+  // .clearCanvas([0.0,1.0,0.0,1.0])
+  .preprocess()
+  .uniform('u_resolution',
             new Float32Array([1.0/w,1.0/h])
         )
-        .uniform('u_raster',0)
-        .uniform('u_textureSize', new Float32Array([w, h]));
+  .uniform('u_raster',textUnit)
+  .uniform('u_textureSize', new Float32Array([w, h]))
+  .uniform('step', 0)
+  .run();
 
-gproc.run();
+let gprocStep1 = gpu.createGPU(gpuEnv,w,h)
+  .geometry(gpu.rectangle(raster.width,raster.height))
+  .attribute('a_vertex',2,'float', 16,0)   // X, Y
+  .attribute('a_texCoord',2, 'float', 16, 8)  // S, T
+  .packWith(program)                       // VAO
+  .texture(gprocStep0.framebuffers['fbo1'], textUnit = 2)
+  // .redirectTo('fbo2','float32',0)
+  .preprocess()
+  .uniform('u_resolution',
+            new Float32Array([1.0/w,1.0/h])
+        )
+  .uniform('u_raster',textUnit)
+  .uniform('u_textureSize', new Float32Array([w, h]))
+  .uniform('step', 1)
+  .run();
 
-// gpu.invert(img.getRaster(),gpuEnv);
+// for(i=0;i<Math.log(h);i++){
+//   gprocStep1.texture(gprocStep1.framebuffers['fbo2'], textUnit = 2)
+//   .redirectTo('fbo2','float32',0)
+//   .preprocess()
+//   .uniform('u_resolution',
+//             new Float32Array([1.0/w,1.0/h])
+//         )
+//   .uniform('u_raster',textUnit)
+//   .uniform('u_textureSize', new Float32Array([w, h]))
+//   .uniform('step', 1)
+//   .run();
+// }
+//
+// gprocStep1.texture(gprocStep1.framebuffers['fbo2'], textUnit = 2)
+// .preprocess()
+// .uniform('u_resolution',
+//           new Float32Array([1.0/w,1.0/h])
+//       )
+// .uniform('u_raster',textUnit)
+// .uniform('u_textureSize', new Float32Array([w, h]))
+// .uniform('step', 3)
+// .run();
